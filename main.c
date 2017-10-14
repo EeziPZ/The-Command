2a3d8aebd70175edca72b89df55d761557860919
EeziPZ <jamiehlake@gmail.com>
2017-10-14 23:17:53 +0200

    Added another ending and loops to make the gameplay better

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ int main()
     char userName[20];
     char reason[20];//Placeholder for when the chief gives the detective a chance to explain himself.
     int task;
+    int complete = 0;
 
     printf("CPP Police Department\nPlease enter your last name to login.\n");
     fgets(userName, 20, stdin);//Gets the name of the player.
@@ -18,6 +19,7 @@ int main()
 
     printf("You chose %d\n", task);
 
+
     if(task == 1){                          //Everything inside this if statement is for task 1, murder on elm street.
 
         printf("Murder on Elm Street accepted.\n\n");
@@ -27,29 +29,61 @@ int main()
 
         scanf("%d", &task);
 
-        if(task == 1){ //Everything in this if statement happens if the player decides to tell the chief what they found.
-            printf("You decide to tell the chief what you found!\n");
-            printf("The chief tells you it must have been a typo and demands you get back to work. He seems a little nervous at your discovery.\nWhat would you like to do?\n\n");
-            printf("1. Investigate your boss, the chief\n2. Visit the bakery where the victim was last seen.\n3. Go for a walk down Elm Street.\n");
+        while (complete == 0){
 
-            scanf("%d", &task);
+            if(task == 1){ //Everything in this if statement happens if the player decides to tell the chief what they found.
+                printf("You decide to tell the chief what you found!\n");
+                printf("The chief tells you it must have been a typo and demands you get back to work. He seems a little nervous at your discovery.\nWhat would you like to do?\n\n");
+                printf("1. Investigate your boss, the chief\n2. Visit the bakery where the victim was last seen.\n3. Go for a walk down Elm Street.\n");
 
-            if (task == 1){
+                scanf("%d", &task);
 
-                printf("One of your fellow officers sees what you are doing and reports it to the chief. The chief calls you to his office and locks the door.\n\n");
-                printf("Chief: It has come to my attention that you do not trust me detective %s\nWhy are you doing this?\n", userName);
-                scanf("%s", reason);
-                printf("\nChief: That is not a good enough excuse. I can't have people like you on my team. You have left me with no choice.");
-                printf("\n\n\nYou got fired, game over!\n\n\n");
-                return 0;
+                if (task == 1){
+
+                    printf("One of your fellow officers sees what you are doing and reports it to the chief. The chief calls you to his office and locks the door.\n\n");
+                    printf("Chief: It has come to my attention that you do not trust me detective %s\nWhy are you doing this?\n", userName);
+                    scanf("%s", reason);
+                    printf("\nChief: That is not a good enough excuse. I can't have people like you on my team. You have left me with no choice.");
+                    printf("\n\n\nYou got fired, game over!\n\n\n");
+                    return 0;
+                }
+            }
+
+            if (task == 2){
+                printf("You decide to visit the bakery where the victim was last seen\n");
+                printf("As you walk towards the bakery, you hear someone call your name, detective %s", userName);
+                printf("You see the captain eating a donut outside the bakery.\n");
+                printf("What do you do?\n\n");
+                printf("1. Tell the chief what you have found.\n2. Ask the captain why he is at the bakery during work hours.\n3. Go for a walk down Elm Street.\n");
+                scanf("%d", &task);
+
+                if (task == 2){
+                    printf("You question the captain\n");
+                    printf("The captain tells you it's his daily routine. That eating the donut helps him stay focused. He then asks why you are there.\n");
+                    scanf("%s", reason);
+                    printf("Captain: I see, well I don't think the bakery will be of any use to you. I suggest you go to the crime scene.\n");
+                    printf("What would you like to do?\n");
+                    printf("1. Tell the chief what you have found\n2. Question the bakery owner\n3. Go for a walk down Elm Street.\n");
+                    scanf("%d", &task);
+
+                    if (task == 2) {
+                        printf("You decide to question the baker\n");
+                        printf("While questioning the baker, the chief tells you again to leave the bakery. The baker insists you take a free pie before you leave\n");
+                        printf("What would you like to do?\n\n1. Investigate the chief\n2. Take the pie and leave.\n3. Go for a walk down Elm Street.\n");
+                        scanf("%d", &task);
+
+                        if (task == 2){
+                            printf("You take the pie, thank the baker and leave\n");
+                            printf("Later that night you eat the pie for dinner\nA few hours later you don't feel too good. You call an ambulance and the chief... but it is too late.\n");
+                            printf("\n\nYou died, Game Over!\n\n\n");
+                            return 0;
+                        }
+                    }
+
+                }
+
             }
         }
-
-        if (task == 2){
-            printf("You decide to visit the bakery where the victim was last seen");
-            return 0;
-        }
-
 
     }
 
